ff6f06c9bdde24aaf659d6f58930bb1fff6ed5df
Kostiantyn Maksymov <160341411+KonstantynSkfyan@users.noreply.github.com>
2025-09-18 11:31:47 +0200

    finished-task-1

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const category = document.getElementById("categories");
+const list = category.querySelectorAll(".item");
+
+console.log(`Number of categories: ${list.length}`);
+
+const getInfo = (list) => {
+  return list.forEach((list) => {
+    console.log(`Category: ${list.querySelector("h2").textContent}`);
+    console.log(`Elements: ${list.querySelectorAll("li").length}`);
+  });
+};
+
+getInfo(list);
