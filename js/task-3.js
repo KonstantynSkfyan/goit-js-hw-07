ca92464fc9b965bfcc0c4e0961a21f37b7950e35
Kostiantyn Maksymov <160341411+KonstantynSkfyan@users.noreply.github.com>
2025-09-21 15:45:49 +0200

    update-homWork

--- js/task-3.js
+++ js/task-3.js
@@ -4,5 +4,5 @@ const outputName = document.getElementById(`name-output`);
 inputName.addEventListener("input", greetName);
 
 function greetName(event) {
-  outputName.textContent = event.currentTarget.value.trim() || "Anonymous";
+  outputName.textContent = event.target.value.trim() || "Anonymous";
 }
